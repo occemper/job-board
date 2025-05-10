f650e5c3a169c0b942861c34487584b67ed20de1
occemper <ravilre22@gmail.com>
2025-05-10 22:39:54 +0300

    006 Layouts Using Components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,11 +21,8 @@
     @endif
 </head>
 
-<body
-    class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<body>
+    {{$slot}}
 </body>
 
 </html>
