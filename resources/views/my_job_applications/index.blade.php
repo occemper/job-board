f8ad0c05a9a7342d75888466ba60593d7600fe10
occemper <ravilre22@gmail.com>
2025-05-19 11:53:15 +0300

    035 My Applications Applicant Count, Average Asking Salary

--- resources/views/my_job_applications/index.blade.php
+++ resources/views/my_job_applications/index.blade.php
@@ -2,7 +2,29 @@
     <x-breadcrumbs class="mb-4" :links="['My job applications' => '#']" />
 
     @forelse ($applications as $application)
-        <x-job-card :job="$application->job"></x-job-card>
+        <x-job-card :job="$application->job">
+            <div class="flex justify-between text-xs text-slate-500 items-center">
+                <div>
+                    <div>
+                        Applied {{ $application->created_at->diffForHumans() }}
+                    </div>
+
+                    <div>
+                        Your asking salary ${{ number_format($application->expected_salary) }}
+                    </div>
+
+                    <div>
+                        Average asking salary
+                        ${{ number_format($application->job->job_applications_avg_expected_salary) }}
+                    </div>
+
+                    <div>
+                        Number of applicants: {{ number_format($application->job->job_applications_count) }}
+                    </div>
+                </div>
+                <div>Rig</div>
+            </div>
+        </x-job-card>
     @empty
     @endforelse
 </x-layout>
